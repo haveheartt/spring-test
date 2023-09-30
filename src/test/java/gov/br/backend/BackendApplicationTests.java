f84faa48405585e9324e119668bd86f4ae8621bb
gui <guilhermev2huehue@gmail.com>
2023-09-30 13:15:56 -0300

    feat: implementing docker-compose for postgres and min.io

---
+++ src/test/java/gov/br/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package gov.br.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
